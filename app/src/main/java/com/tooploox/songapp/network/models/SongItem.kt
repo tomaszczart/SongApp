f194cc1678668e74b47371db47818df59e5218ee
Tomasz Czart <czart.tomasz@gmail.com>
2019-08-01 16:44:09 +0200

    Added song item and model.

---
+++ app/src/main/java/com/tooploox/songapp/network/models/SongItem.kt
@@ -0,0 +1,6 @@
+package com.tooploox.songapp.network.models
+
+/**
+ * Item that will be displayed on the songs list.
+ */
+data class SongItem(val artist: String, val title: String, val releaseYear: Int)
