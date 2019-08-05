3967c0ea44828fe040dee078564d374e93fb2394
Tomasz Czart <czart.tomasz@gmail.com>
2019-08-06 01:43:39 +0200

    Added test classes.

---
+++ app/src/androidTest/java/com/tooploox/songapp/network/SongsRepositoryTest.kt
@@ -0,0 +1,12 @@
+package com.tooploox.songapp.network
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SongsRepositoryTest {
+
+    @Test
+    fun search() {
+    }
+}
---
+++ app/src/androidTest/java/com/tooploox/songapp/songsScreen/viewModel/SongsActivityViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.tooploox.songapp.songsScreen.viewModel
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SongsActivityViewModelTest {
+
+    @Test
+    fun searchSong() {
+    }
+}
